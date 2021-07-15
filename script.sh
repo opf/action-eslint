cc9f3b0c169d015fe0a4ed9872dda479f65d4ee4
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2021-07-15 16:53:39 +0200

    Change newlines to spaces and don't put all the files as if they were one filename

--- script.sh
+++ script.sh
@@ -17,12 +17,12 @@ npm install
 echo '::endgroup::'
 
 echo '::group:: Getting changed files list'
-CHANGED_FILES=$(git diff --name-only "${BASE_REF}..${HEAD_REF}" '**/*.js' '**/*.ts' | cut -f 2-1000 -d '/')
+CHANGED_FILES=$(git diff --name-only "${BASE_REF}..${HEAD_REF}" '**/*.js' '**/*.ts' | cut -f 2-1000 -d '/' | tr '\n' ' ')
 echo "$CHANGED_FILES"
 echo '::endgroup::'
 
 echo '::group:: Running eslint with reviewdog 🐶 ...'
-"$(npm bin)"/eslint -f="${ESLINT_FORMATTER}" -c .eslintrc.js "${CHANGED_FILES}" | reviewdog -f=rdjson \
+"$(npm bin)"/eslint -f="${ESLINT_FORMATTER}" -c .eslintrc.js ${CHANGED_FILES} | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-review}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
